6acb42fed91380d7124620967a3283f01271d058
Bryceson Laing <bklaing2@gmail.com>
2026-06-05 15:35:35 -0500

    Revert "set up drizzle"
    This reverts commit 7ceb749bb54faa77f50c9b39adb55ee80217fd82.

--- src/lib/server/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { DATABASE_URL } from "$env/static/private";
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-const db = drizzle({ connection: DATABASE_URL, casing: 'snake_case' });
-
-export default db
-export type Database = typeof db
